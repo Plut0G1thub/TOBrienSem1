61e89b194d8f9f3d2faed78b18f8a38f0fa3d62c
MikeGardner <52153@student.btech.edu>
2026-08-31 09:41:54 -0600

    Day 1 8/31/2026
    Finished:
    - Varaibles and Data Types
    
    Partially Completed:
    - Statements Expressions and Operators | 2/3
    
    Notes:
    N/A

---
+++ Previous Assignments/Var and Data.cs
@@ -0,0 +1,82 @@
+﻿/* ~~~~~~~~~~~ Exercise 1: Swapping Variables ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 1: Swapping Variables ~~~~~~~~~~~ */");
+int a = 4;
+int b = 62;
+int temp;
+
+Console.WriteLine($"a: {a} b: {b}");
+
+temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine($"a: {a} b: {b}");
+
+Console.WriteLine("");
+/* ~~~~~~~~~~~ Exercise 2: Personal Information Formatting ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 2: Personal Information Formatting ~~~~~~~~~~~ */");
+
+string name = "Trenton";
+int age = 17;
+string hometown = "Tremtonton";
+
+Console.WriteLine($"{name} is from {hometown}, and is {age} years old.");
+
+Console.WriteLine("");
+/* ~~~~~~~~~~~ Exercise 3: Temperature Conversion ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 3: Temperature Conversion ~~~~~~~~~~~ */");
+
+double celsius;
+double fahrenheit;
+
+Console.WriteLine("Input a Celsius Tempurature Value");
+string userValue = Console.ReadLine();
+
+int userInt;
+int.TryParse(userValue, out userInt);
+
+celsius = (double)userInt;
+
+fahrenheit = ((celsius * 9) / 5) + 32;
+
+Console.WriteLine($"{celsius} degrees Celsius is equivalent to {fahrenheit} degrees Fahrenheit");
+
+Console.WriteLine("");
+
+/* ~~~~~~~~~~~ Exercise 4: Area of a Rectangle ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 4: Area of a Rectangle ~~~~~~~~~~~ */");
+
+double width;
+double height;
+
+Console.WriteLine("Input a width for a rectangle:");
+string userInput = Console.ReadLine();
+int userRectInt;
+int.TryParse(userInput, out userRectInt);
+width = (double)userRectInt;
+
+Console.WriteLine("Input a height for a rectangle:");
+userInput = Console.ReadLine();
+int.TryParse(userInput, out userRectInt);
+height = (double)userRectInt;
+
+double area = width * height;
+double perimeter = (width * 2) + (height * 2);
+
+Console.WriteLine($"The rectangle is width {width} and height {height}. Its perimeter is {perimeter} and its area is {area}.");
+
+Console.WriteLine("");
+/* ~~~~~~~~~~~ Exercise 5: Data Formatting ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 5: Data Formatting ~~~~~~~~~~~ */");
+
+int integer = 123;
+double doubling = 24;
+char character = 'A';
+bool boolean = false;
+string text = "Lorem Ipsum";
+
+Console.WriteLine($"{integer.GetType()}: {integer}");
+Console.WriteLine($"{doubling.GetType()}: {doubling}");
+Console.WriteLine($"{character.GetType()}: {character}");
+Console.WriteLine($"{boolean.GetType()}: {boolean}");
+Console.WriteLine($"{text.GetType()}: {text}");
---
+++ Project/Program.cs
@@ -0,0 +1,77 @@
+﻿/* ~~~~~~~~~~~ Exercise 1: Statements vs Expressions ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 1: Statements vs Expressions ~~~~~~~~~~~ */");
+
+int calculation = 34 * 17 - 12 / 6;
+calculation -= (9 * 3);
+Console.WriteLine(calculation + " | calculation -= (9 * 3);");
+
+Console.WriteLine("");
+
+/* ~~~~~~~~~~~ Exercise 2: Expression Placement and Usage ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 2: Expression Placement and Usage ~~~~~~~~~~~ */");
+
+// Expression 1: (4 * (7 - 9) + 11) / 3
+Console.WriteLine("Expression 1: (4 * (7 - 9) + 11) / 3");
+
+string result = Convert.ToString((4 * (7 - 9) + 11) / 3);
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+// Expression 2: (3 == 2 && 4 > 1) || false
+Console.WriteLine("Expression 2: (3 == 2 && 4 > 1) || false");
+
+result = Convert.ToString((3 == 2 && 4 > 1) || false);
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+//Expression 3: (32.1 - 3.445) * 17.7 / (2.38 + 21.9)
+Console.WriteLine("Expression 3: (32.1 - 3.445) * 17.7 / (2.38 + 21.9)");
+
+result = Convert.ToString((32.1 - 3.445) * 17.7 / (2.38 + 21.9));
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+/* ~~~~~~~~~~~ Exercise 3: Order of Operations ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 3: Order of Operations ~~~~~~~~~~~ */");
+
+// Expression 1: (4 * (7 - 9) + 11) / 3
+Console.WriteLine("Expression 1: 4 * 7 - 9 + 11 / 3");
+
+result = Convert.ToString(4 * (7 - 9) + 11 / 3);
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+// Expression 2: (3 == 2 && 4 > 1) || false
+Console.WriteLine("Expression 2: 3 == 2 && 4 > 1 || false");
+
+result = Convert.ToString(3 == 2 && 4 > 1 || false);
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+//Expression 3: (32.1 - 3.445) * 17.7 / (2.38 + 21.9)
+Console.WriteLine("Expression 3: 32.1 - 3.445 * 17.7 / 2.38 + 21.9");
+
+result = Convert.ToString(32.1 - 3.445 * 17.7 / 2.38 + 21.9);
+Console.WriteLine($"Result: {result}");
+
+Console.WriteLine("");
+
+/* ~~~~~~~~~~~ Exercise 4: Type-Inferred Variables ~~~~~~~~~~~ */
+Console.WriteLine("/* ~~~~~~~~~~~ Exercise 4: Type-Inferred Variables ~~~~~~~~~~~ */");
+
+int declared = 28;
+var inferred = declared * 2.13;
+bool isDeclared = true;
+var isInferred = 2 > 3;
+var alsoInferred = $"{inferred}";
+Console.WriteLine(declared.GetType());
+Console.WriteLine(inferred.GetType());
+Console.WriteLine(isDeclared.GetType());
+Console.WriteLine(isInferred.GetType());
+Console.WriteLine(alsoInferred.GetType());
+
